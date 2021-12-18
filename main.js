7bc314b6c16a4d308938e41a1114562b2b5f7064
ezequiel magi <ezequielmaggi@gmail.com>
2021-12-18 17:42:21 -0300

    list forma mas sencilla

--- main.js
+++ main.js
@@ -1,32 +1,20 @@
-class Accordion{
-  constructor(selector, multiple){
-    this.accordion = document.querySelectorAll(selector)
-    this.multiple = multiple
-    this.bindEvents()
-  }
+const bloque = document.querySelectorAll('.bloque')
+const h2 = document.querySelectorAll('h2')
 
-  bindEvents(){
-    document.querySelectorAll(".item header").forEach(itemHeader =>{
-      itemHeader.addEventListener('click', ()=>{
-        let item = itemHeader.parentNode
+//Cuando haga CLICK en h2
+  //quitar la clase activo de todos los bloques
+  //vamos a añadir la clase activo del bloque con la posicion del h2
 
-        item.classList.toggle('active')
-      })
-    })
-  }
+h2.forEach( ( cadaH2 , i ) => {
 
-  validateMultiple(selectedItem){
-    if(this.multiple) return;
-    this.accordion.querySelectorAll('.item').forEach(item =>{
-      if(selectedItem == item) return;
-      item.classList.remove('active')
-    })
-  }
+  
+  h2[i].addEventListener('click', ()=>{
+    
+    bloque.forEach( ( cadaBloque , i )=>{
+      bloque[i].classList.remove('activo')
+    } )
 
-}
+    bloque[i].classList.add('activo')
 
-
-(function(){
-
-  new Accordion('.accordion', false)
-})()
+  })
+})
